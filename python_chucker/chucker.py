45b14a89421532abf7890c68e2d6d1efa87d2505
philkuz <superpker@gmail.com>
2015-10-10 08:38:23 -0700

    Made the chucker chuck-to-python translator.

---
+++ python_chucker/chucker.py
@@ -0,0 +1,21 @@
+class Chucker:
+    def __init__(self, name='foo.ck'):
+        self.set_name(name)
+    def set_name(self, name):
+        # add extension if it's not there
+        if '.ck' not in name:
+            self.name = name+'.ck'
+        # don't need to if it is there
+        else:
+            self.name = name
+    def write_chuck(self, chucks):
+        assert type(chucks) == list, "Chucks argument should be a list, not a {0}".format(type(chucks))
+        thefile = open(self.name, 'w')
+        for chuck_line in chucks:
+            thefile.write("%s\n" % chuck_line)
+        thefile.close()
+    def execute_chuck(self):
+        import subprocess
+        command = 'chuck ' + self.name
+        chuck_cmd = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        output = process.communicate()[0]
