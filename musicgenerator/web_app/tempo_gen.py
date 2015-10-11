a3f85166f37406bf8d99a780d5d74a43f3ebec70
Kunal Gosar <gosarkunal9@gmail.com>
2015-10-10 18:14:19 -0700

    Added BPM Calculations to Swift; Manual input possibility to webserver

--- musicgenerator/web_app/tempo_gen.py
+++ musicgenerator/web_app/tempo_gen.py
@@ -1,5 +1,5 @@
 def tempo(timestamps, config_index):
     """Pull timestamps input from mobile device, output tempo value for music generator web app"""
     timestamps = [float(x) for x in timestamps]
-    average_bpm = ((max(timestamps) - min(timestamps))/len(timestamps))*60
+    average_bpm = (len(timestamps)/(max(timestamps) - min(timestamps)))*60
     return average_bpm
