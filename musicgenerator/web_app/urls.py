a3e5ddc5751c81a9a1064a956c278064ea68ecd1
Arda Sahiner <sahinera@gmail.com>
2015-10-11 09:31:54 -0700

    working on python scripts

--- musicgenerator/web_app/urls.py
+++ musicgenerator/web_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.aboutus, name='aboutus')
+    #url(r'^$', views.aboutus, name='aboutus'),
+    url(r'^/api/newgenre/(?P<some_data>[\w-]+)/$', views.python_query, name='python_query')
 ]
--- musicgenerator/web_app/views.py
+++ musicgenerator/web_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.utils import simplejson
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def aboutus(request):
     return render(request, 'aboutus.html')
+def python_query(request, some_data):
+
+    return HttpResponse(simplejson.dumps(some_data), mimetype='application/json')
+    #some_data=request.GET.get('some_data')
+    #return some_data
