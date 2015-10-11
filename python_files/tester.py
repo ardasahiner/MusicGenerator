527d6b7a13b11a46dc6d9ca85a9e82a177b48de7
Arda Sahiner <sahinera@gmail.com>
2015-10-11 08:43:48 -0700

    Fixed errors with importing midi.

--- python_files/tester.py
+++ python_files/tester.py
@@ -2,5 +2,6 @@ import scalable_music_generator as scalable
 import os
 genre_names=['mozart', 'jazz', 'gamemusic4']
 
+
 jazz_generator=scalable.Scalable_Midi_Generator()
 print(jazz_generator.choose_midi(154))
