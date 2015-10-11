e5bf360a4a513ea09115e0c36a89d1d70b2aebfe
Arda Sahiner <sahinera@gmail.com>
2015-10-11 01:18:21 -0700

    Added default values for scalable_music_generator.py constructor

--- python_files/tester.py
+++ python_files/tester.py
@@ -2,5 +2,5 @@ import scalable_music_generator as scalable
 import os
 genre_names=['mozart', 'jazz', 'gamemusic4']
 
-jazz_generator=scalable.Scalable_Midi_Generator(50, 250, genre_names[1])
+jazz_generator=scalable.Scalable_Midi_Generator()
 print(jazz_generator.choose_midi(154))
